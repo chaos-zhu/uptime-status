e3c3113ffbadd44cf040ba185911b979da9ddb0c
chaos-zhu <ggchaos1022@gmail.com>
2023-03-20 18:23:10 +0800

    update

--- public/config.js
+++ public/config.js
@@ -6,10 +6,9 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm793956511-e93b8cebf584c89a0af8fd36', // ghproxy
+    'm793956510-3d1bc26219843c10d03d664f', // easynode
+    'm793956507-9466e492caab9aea33135087', // blog
   ],
 
   // 日志天数
@@ -21,16 +20,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Blog',
+      url: 'https://zhulijun.club/'
     },
     {
       text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      url: 'https://github.com/chaos-zhu'
     },
   ],
 };
